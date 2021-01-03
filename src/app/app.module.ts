5657d192a71e5fbdbf2a5720f23046a2bd7eea40
Klavdija <kv8904@student.uni-lj.si>
2021-01-03 18:46:24 +0100

    things deleted, tremolo value changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
 import { Sec2minPipe } from './shared/sec2min.pipe';
 import { EqualizerComponent } from './song/equalizer/equalizer.component';
 import { SeekComponent } from './song/seek/seek.component';
@@ -18,7 +17,6 @@ import { VisualizerComponent } from './song/visualizer/visualizer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
     SongComponent,
     SeekComponent,
     Sec2minPipe,
